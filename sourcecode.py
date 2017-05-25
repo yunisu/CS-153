9828393d8be7a44d149a8a573429be4e89786720
yunisu <eungelcruz97@gmail.com>
2017-05-26 02:36:16 +0900

    Update sourcecode.py

--- sourcecode.py
+++ sourcecode.py
@@ -62,30 +62,50 @@ def add_or_sub(input1, input2):
 
 	final_elements = []
 
+	print "\n\nFirst, we truncate 0s to the inputs to make them \nof the same length and to avoid confusion during\ncomputations. It becomes: "
 	input1, input2 = add_zero(input1, input2)
+	print "\nA(x): ",
+	for i in input1:
+		print i,
+	print "\nB(x): ",
+	for i in input2:
+		print i,
+	print '\n'
 
 	maxlen = max(len_1, len_2)
 
 	i = maxlen - 1
-
+	counter = 1
+	print "Then, we get the binary equivalent of each element,\nalso truncating 0s to avoid confusion when computing.\n\n"
 	while i >= 0:
+		print "Now, we have these elements from column", counter, "(from the right):\n"
+		counter += 1
 		bin_1 = bin(int(input1[i]))
 		bin_2 = bin(int(input2[i]))
 		bin_1 = bin_1[2:]
 		bin_2 = bin_2[2:]
 		bin_1, bin_2 = add_zero_in_bin(bin_1, bin_2)
-		element = bin_to_decimal(xor(bin_1, bin_2))
+		print bin_1 + " - the binary equivalent of " + input1[i] 
+		print bin_2 + " - the binary equivalent of " + input2[i]
+		print "\nWe perform Logical Exclusive-Or (XOR) to elements of the same column."
+		print "\nSo here, we perform " + bin_1 +" XOR " + bin_2 + '\n'
+		semi_final = xor(bin_1, bin_2)
+		print "-----> " + bin_1 +" XOR " + bin_2 + " = " +semi_final
+		element = bin_to_decimal(semi_final)
+		print "\nWe then convert the result for this column back to original base (10):" 
+		print "\n-----> The result " + semi_final + " becomes " + str(element) + " and this becomes the final answer\n       for this column.\n\n"
 		final_elements.append(element)
 		i -= 1
+
 	return final_elements[::-1]
 
 while True:
 
-	print '\n', 25 * "-", "M E N U", 24*"-", '\n'
+	print '\n',  30* "-", "M E N U", 30*"-", '\n'
 
-	input1 = "12 1"
-	input2 = "4"
-	input3 = "1 0 0 1 1"
+	input1 = raw_input("Enter A(x):")
+	input2 = raw_input("Enter B(x):")
+	input3 = raw_input("Enter P(x):")
 
 	print_menu()
 
@@ -119,7 +139,9 @@ while True:
 		bool_answer = all(int(i) < max_value  for i in full_list)
 		
 		if bool_answer is False:
-			print "A(x):", 
+			print '\n', 26 * "-", "s o l u t i o n", 26*"-"
+			print "Here are the given: "
+			print "\nA(x):", 
 			for i in input1:
 				print i,
 			print "\nB(x):",
@@ -127,14 +149,31 @@ while True:
 				print i,
 			print "\nAnswer: Maximum value in (A,B) is", max_value - 1, "for this P(x)"
 		else:
-			print "A(x):", 
+			print '\n', 26 * "-", "s o l u t i o n", 26*"-"
+			print "Here are the given: "
+			print "\nA(x):", 
 			for i in input1:
 				print i,
 			print "\nB(x):",
 			for i in input2:
 				print i,
 			final = add_or_sub(input1, input2)
-			print "\nAnswer",
+
+			print "\n ", 
+			for i in input1:
+				print i,
+			print " - A(x)"
+			print "\n+",
+			for i in input2:
+				print i,
+			print " - B(x)"
+			print "\n ", m*2*'-'
+			print " ", 
+			for i in final:
+				print i,
+			print " - Result\n"
+			print '\n'
+			print "\nTherefore, the result for this operation with \nthe given A(x) and B(x) above is:",
 			for i in final:
 				print i,
 			print '\n'
@@ -166,7 +205,9 @@ while True:
 		bool_answer = all(int(i) < max_value  for i in full_list)
 		
 		if bool_answer is False:
-			print "A(x):", 
+			print '\n', 26 * "-", "s o l u t i o n", 26*"-"
+			print "Here are the given: "
+			print "\nA(x):", 
 			for i in input1:
 				print i,
 			print "\nB(x):",
@@ -174,14 +215,31 @@ while True:
 				print i,
 			print "\nAnswer: Maximum value in (A,B) is", max_value - 1, "for this P(x)"
 		else:
-			print "A(x):", 
+			print '\n', 26 * "-", "s o l u t i o n", 26*"-"
+			print "Here are the given: "
+			print "\nA(x):", 
 			for i in input1:
 				print i,
 			print "\nB(x):",
 			for i in input2:
 				print i,
 			final = add_or_sub(input1, input2)
-			print "\nAnswer",
+
+			print "\n ", 
+			for i in input1:
+				print i,
+			print " - A(x)"
+			print "\n-",
+			for i in input2:
+				print i,
+			print " - B(x)"
+			print "\n ", m*2*'-'
+			print " ", 
+			for i in final:
+				print i,
+			print " - Result\n"
+			print '\n'
+			print "\nTherefore, the result for this operation with \nthe given A(x) and B(x) above is:",
 			for i in final:
 				print i,
 			print '\n'
